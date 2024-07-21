a33eed6777133ff63c12291222b7842ebb87c3c9
Griffin Sectio <grifin.dwi13@gmail.com>
2024-07-21 11:33:00 +0700

    update start.go

--- cmd/start.go
+++ cmd/start.go
@@ -4,6 +4,7 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -21,6 +22,7 @@ var env = `/usr/bin/env `
 var sysctl = `/etc/sysctl.conf`
 var torrc = `/etc/tor/torgorc`
 var resolvconf = `/etc/resolv.conf`
+var tor = `/usr/bin/tor`
 
 var disable_ipv6 = `net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1`
@@ -35,14 +37,14 @@ RunAsDaemon 1`
 var resolvconfig = `nameserver 127.0.0.1`
 
 var NON_TOR = `192.168.1.0/24 192.168.0.0/24`
-var iptables_rules = fmt.Sprint(`NON_TOR="%[0]s"
-TOR_UID=%[1]s
+var iptables_rules = fmt.Sprintf(`NON_TOR="%[1]s"
+TOR_UID=%[2]s
 TRANS_PORT="9040"
 
 iptables -F
 iptables -t nat -F
 
-iptables -t nat -A OUTPUT -m owner --uid-owner %[1]s -j RETURN
+iptables -t nat -A OUTPUT -m owner --uid-owner %[2]s -j RETURN
 iptables -t nat -A OUTPUT -p udp --dport 53 -j REDIRECT --to-ports 5353
 for NET in $NON_TOR 127.0.0.0/9 127.128.0.0/10; do
  iptables -t nat -A OUTPUT -d $NET -j RETURN
@@ -53,7 +55,7 @@ iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 for NET in $NON_TOR 127.0.0.0/8; do
 iptables -A OUTPUT -d $NET -j ACCEPT
 done
-iptables -A OUTPUT -m owner --uid-owner %[1]s -j ACCEPT
+iptables -A OUTPUT -m owner --uid-owner %[2]s -j ACCEPT
 iptables -A OUTPUT -j REJECT
 
 iptables -A FORWARD -m string --string "BitTorrent" --algo bm --to 65535 -j DROP
@@ -84,10 +86,9 @@ var startCmd = &cobra.Command{
 
 		if !strings.Contains(string(content), disable_ipv6) {
 			fmt.Println("Disabling IPv6...")
-			backup, err := os.Create("/etc/sysctl.conf.bak")
+			backup, err := os.Create(sysctl + ".bak")
 			if err != nil {
 				log.Fatalf("Unable to create backup file: %v", err)
-
 			}
 			backup.WriteString(string(content))
 
@@ -96,65 +97,84 @@ var startCmd = &cobra.Command{
 				log.Fatalf("Unable to write to %s: %v", sysctl, err)
 			}
 
-			out, err := exec.Command("/usr/sbin/sysctl -p").Output()
+			out, err := exec.Command(env + "sysctl -p").Output()
 			if err != nil {
 				log.Fatalf("Unable to reload sysctl.conf: %v", err)
 			}
 			fmt.Println(string(out))
-
-			if _, err := os.Stat(torrc); err == nil {
-				content, err := os.ReadFile(torrc)
-				if err != nil {
-					log.Fatalf("Unable to read %s: %v", torrc, err)
-				}
-				if strings.Contains(string(content), torrcconfig) {
-					fmt.Println("torgo configuration is already configured")
-				} else {
-					file, err := os.OpenFile(torrc, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
-					if err != nil {
-						log.Fatalf("unable to open %s: %v", torrc, err)
-					}
-					defer file.Close()
-					file.WriteString(torrcconfig)
-				}
-			}
-
-			content, err := os.ReadFile(resolvconf)
-			if err != nil {
-				log.Fatalf("unable to open file: %v", err)
-			}
-			if !strings.Contains(string(content), resolvconfig) {
-				file, err := os.OpenFile(resolvconf, os.O_WRONLY|os.O_CREATE, 0644)
-				if err != nil {
-					log.Fatalf("unable to open file %s: %v", resolvconf, err)
-				}
-				defer file.Close()
-
-				_, err = file.WriteString(resolvconfig)
-				if err != nil {
-					log.Fatalf("Unable to write to %s: %v", resolvconf, err)
-				}
-
-				fmt.Println("resolv.conf is configured")
-			} else {
-				fmt.Println("resolv.conf already configured")
-			}
-
-			if _, err := os.Stat("/usr/bin/tor"); err == nil {
-				exec.Command(env + "systemctl stop tor")
-				exec.Command("fuser -k 9051/tcp > /dev/null 2>&1")
-
-				exec.Command(fmt.Sprintf("sudo -u %s tor -f %s > /dev/null'.format(TOR_USER, Torrc)", TOR_USER, torrc))
-
-			} else {
-				log.Fatalf("Unable to locate tor, is it installed? %v", err)
-			}
-
-			exec.Command(iptables_rules)
-
 		} else {
 			fmt.Println("IPv6 is already disabled")
 		}
+
+		if _, err := os.Stat(torrc); err == nil {
+			content, err := os.ReadFile(torrc)
+			if err != nil {
+				log.Fatalf("Unable to read %s: %v", torrc, err)
+			}
+			if !strings.Contains(string(content), torrcconfig) {
+				file, err := os.OpenFile(torrc, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+				if err != nil {
+					log.Fatalf("unable to open %s: %v", torrc, err)
+				}
+				defer file.Close()
+				file.WriteString(torrcconfig)
+			} else {
+				fmt.Println("torgo configuration is already configured")
+			}
+		}
+
+		content, err = os.ReadFile(resolvconf)
+		if err != nil {
+			if errors.Is(err, os.ErrNotExist) {
+				fmt.Printf("%s not exist, creating one...\n", resolvconf)
+
+				file, err = os.OpenFile(resolvconf, os.O_WRONLY|os.O_CREATE, 0644)
+				if err != nil {
+					log.Fatalf("unable to create %s: %v", resolvconf, err)
+				}
+				content, err = os.ReadFile(resolvconf)
+				if err != nil {
+					log.Fatalf("unable to read %s: %v", resolvconf, err)
+				}
+				file.WriteString("")
+			} else {
+				log.Fatalf("unable to open file: %v", err)
+			}
+		}
+		if !strings.Contains(string(content), resolvconfig) {
+			file, err := os.OpenFile(resolvconf+".bak", os.O_WRONLY|os.O_CREATE, 0644)
+			if err != nil {
+				log.Fatalf("unable to create file %s: %v", resolvconf, err)
+			}
+			defer file.Close()
+			file.WriteString(string(content))
+
+			file, err = os.OpenFile(resolvconf, os.O_WRONLY|os.O_CREATE, 0644)
+			if err != nil {
+				log.Fatalf("unable to open file %s: %v", resolvconf, err)
+			}
+			defer file.Close()
+
+			_, err = file.WriteString(resolvconfig)
+			if err != nil {
+				log.Fatalf("Unable to write to %s: %v", resolvconf, err)
+			}
+
+			fmt.Println("resolv.conf is configured")
+		} else {
+			fmt.Println("resolv.conf already configured")
+		}
+
+		if _, err := os.Stat(tor); err == nil {
+			exec.Command(env + "systemctl stop tor")
+			exec.Command("fuser -k 9051/tcp > /dev/null 2>&1")
+
+			exec.Command(fmt.Sprintf("sudo -u %s tor -f %s > /dev/null", TOR_USER, torrc))
+
+		} else {
+			log.Fatalf("Unable to locate tor, is it installed? %v", err)
+		}
+		exec.Command(iptables_rules)
 		fmt.Println("Something happened!")
 	},
 }
